63552d60fe051d844ef1a3431f29cf00b16ab70c
Kamo <vartanyandg@gmail.com>
2024-09-09 18:26:10 +0300

    FTBE: Setup OtDushiApiController | OtDushiApiController

--- app/Http/Controllers/OtDushiApiController.php
+++ app/Http/Controllers/OtDushiApiController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\OtDushiAiServiceInterface;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class OtDushiApiController extends Controller
 {
-    //
+    /**
+     * @var OtDushiAiServiceInterface
+     */
+    private OtDushiAiServiceInterface $otDushiAi;
+
+    /**
+     * Constructor.
+     *
+     * @param OtDushiAiServiceInterface $otDushiAi
+     */
+    public function __construct(OtDushiAiServiceInterface $otDushiAi)
+    {
+        $this->otDushiAi = $otDushiAi;
+    }
+
+    /**
+     * Describe an image using OpenAiService.
+     *
+     * This method sends an image to OpenAiService for analysis and returns the response from OpenAiService.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function getAiSpreads(Request $request): JsonResponse
+    {
+        $imagesUrl = $request->input('images');
+        $prompt = $request->input('prompt');
+
+        if (empty($request->input('images')) || empty($request->input('prompt'))) {
+            return response()->json(
+                ['error' => 'Image URL and prompt are required'],
+                400
+            );
+        }
+
+        try {
+            $response = $this->otDushiAi->getSpreadsFromOpenAi([$imagesUrl], $prompt);
+            return response()->json($response->getData());
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
