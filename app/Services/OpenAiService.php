7739ebed2a26f0e6fb9332ed2b5173dd8e84b1dc
Kamo <vartanyandg@gmail.com>
2024-09-09 17:39:56 +0300

    FTBE: Create OpenAiService | NEW OpenAiService

---
+++ app/Services/OpenAiService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+use OpenAI\Laravel\Facades\OpenAI;
+use OpenAI\Responses\Chat\CreateResponse;
+
+/**
+ * Class for interacting with OpenAI.
+ * This class implements the OpenAiServiceInterface and provides methods for working with OpenAI.
+ *
+ * @package App/Services
+ */
+class OpenAiService implements OpenAiServiceInterface
+{
+    /**
+     * Creates a request to OpenAI with the specified parameters.
+     *
+     * This method creates a request to OpenAI with the specified parameters and returns the response from OpenAI.
+     *
+     * @param string $model The OpenAI model to use for processing the request.
+     * @param array $messages The array of messages to send to OpenAI.
+     * @param int $maxTokens The maximum number of tokens that can be in the response from OpenAI.
+     *
+     * @return CreateResponse The response from OpenAI as a CreateResponse object.
+     *
+     * @throws Exception If the request to OpenAI fails.
+     */
+    public function createCommonRequest(string $model, array $messages, int $maxTokens): CreateResponse
+    {
+        return OpenAI::chat()->create([
+            'model' => $model,
+            'messages' => $messages,
+            'max_tokens' => $maxTokens,
+        ]);
+    }
+}
