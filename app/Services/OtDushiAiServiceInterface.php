7f0dbe555809e9054ae94b9396027a6df616c7bb
Kamo <vartanyandg@gmail.com>
2024-09-09 17:50:32 +0300

    FTBE: Create OtDushiAiServiceInterface | NEW OtDushiAiServiceInterface

---
+++ app/Services/OtDushiAiServiceInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use App\ValueObject\OtDushiAiSpreadsResult;
+use Exception;
+
+/**
+ * Interface for interacting with OtDushi Ai Service.
+ * This interface defines the methods that must be implemented by a class working with OtDushiAiService.
+ *
+ * @package App\Services
+ */
+interface OtDushiAiServiceInterface
+{
+    /**
+     * Sends an array of images to OpenAiService for processing.
+     *
+     * This method sends an array of images to OpenAiService for processing and returns the response from OpenAiService.
+     *
+     * @param array $images The array of images to send to OpenAiService.
+     * @param string $prompt The text for Prompt
+     * @return OtDushiAiSpreadsResult Spreads Result.
+     *
+     * @throws Exception If the request to OpenAiService fails.
+     */
+    public function getSpreadsFromOpenAi(array $images, string $prompt): OtDushiAiSpreadsResult;
+}
