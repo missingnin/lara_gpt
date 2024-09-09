1d53c204ed81b7a5992f9b9fbcfcd2cee23e2b22
Kamo <vartanyandg@gmail.com>
2024-09-09 17:49:24 +0300

    FTBE: Create OtDushiAiSpreadsResult | NEW OtDushiAiSpreadsResult

---
+++ app/ValueObject/OtDushiAiSpreadsResult.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\ValueObject;
+
+/**
+ * Class for representing the result of a spreads request to OpenAiService.
+ *
+ * This class provides a convenient way to access the data returned by OpenAiService.
+ *
+ * @package App\Services
+ */
+class OtDushiAiSpreadsResult
+{
+    /**
+     * @var array
+     */
+    private array $data;
+
+    /**
+     * Constructor.
+     *
+     * @param array $data The data returned by OpenAiService.
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Gets the data returned by OpenAiService.
+     *
+     * @return array The data returned by OpenAiService.
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Gets the spreads returned by OpenAiService.
+     *
+     * @return array The spreads returned by OpenAiService.
+     */
+    public function getSpreads(): array
+    {
+        return $this->data['data'] ?? [];
+    }
+
+    /**
+     * Gets the error message returned by OpenAiService.
+     *
+     * @return string|null The error message returned by OpenAiService.
+     */
+    public function getErrorMessage(): ?string
+    {
+        return $this->data['error'] ?? null;
+    }
+}
