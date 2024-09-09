43c050547c42af14451099a9e359507fa84132d2
Kamo <vartanyandg@gmail.com>
2024-09-09 18:53:29 +0300

    FTBE: Setup Get Spreads Route | api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\OtDushiApiController;
 use App\Http\Controllers\WidgetAiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +22,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('ot_dushi')->group(function () {
+    Route::post('/get_spreads', [OtDushiApiController::class, 'getAiSpreads'])->name('od.getSpreads');
+});
 
 Route::get('/widget_settings', [AdminController::class, 'getWidgetSettings'])->name('getWidgetSettings');
 Route::post('/widget_settings/update', [AdminController::class, 'updateWidgetSettings'])->name('updateWidgetSettings');
