0c3928af66454ad411ae2506075decaefdceacad
missingnin <vartanyandg@gmail.com>
2024-10-11 18:18:24 +0300

    FTBE: Create ProcessType Contant | NEW OtDushiAiProcessTypes.php

---
+++ app/Constants/OtDushiAiProcessTypes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constants;
+
+/**
+ * Class OtDushiAiProcessTypes
+ *
+ * Defines a set of process types for OtDushi AI processor
+ */
+class OtDushiAiProcessTypes
+{
+    const GET_AI_IMAGES_DESCRIPTION = 1;
+    const GET_AI_SPREADS_GROUPS = 2;
+}
