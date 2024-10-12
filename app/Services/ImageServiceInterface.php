57629c8086674e10dcf263b1c6328a43a4cb1d52
missingnin <vartanyandg@gmail.com>
2024-10-12 22:02:58 +0300

    FTBE: Add New Method To Interface | ImageServiceInterface::isImageUrlAccessible()

--- app/Services/ImageServiceInterface.php
+++ app/Services/ImageServiceInterface.php
@@ -18,4 +18,14 @@ interface ImageServiceInterface
      * @return Collection
      */
     public function syncImages(array $images, Product $product): Collection;
+
+    /**
+     * Check if an image is accessible by its URL.
+     *
+     * Sends a HEAD request to the image URL and checks if the response status code is 200
+     *
+     * @param string $imageUrl The image URL to check
+     * @return bool True if the image is accessible, false otherwise
+     */
+    public function isImageUrlAccessible(string $imageUrl): bool;
 }
