7abe27f9d05c2f8be0c27051fc052ed7452abffd
missingnin <vartanyandg@gmail.com>
2024-10-12 18:26:41 +0300

    FTBE: Create Abstract OpenAiResult | OpenAiResult.php

--- app/ValueObject/ImageDescriptionResult.php
+++ app/ValueObject/ImageDescriptionResult.php
@@ -2,57 +2,13 @@
 
 namespace App\ValueObject;
 
+use App\ValueObject\OpenAiResult;
+
 /**
- * Class for representing the result of a spreads request to OpenAiService.
- *
- * This class provides a convenient way to access the data returned by OpenAiService.
- *
- * @package App\Services
+ * Class for representing the result of an image description request to OpenAiService.
  */
-class ImageDescriptionResult
+class ImageDescriptionResult extends OpenAiResult
 {
-    /**
-     * @var array
-     */
-    private array $data;
 
-    /**
-     * Constructor.
-     *
-     * @param array $data The data returned by OpenAiService.
-     */
-    public function __construct(array $data)
-    {
-        $this->data = $data;
-    }
-
-    /**
-     * Gets the image description returned by OpenAiService.
-     *
-     * @return string The image description returned by OpenAiService.
-     */
-    public function getImageDescription(): string
-    {
-        return $this->data['content'][0]['text'] ?? '';
-    }
-
-    /**
-     * Gets the error message returned by OpenAiService.
-     *
-     * @return string|null The error message returned by OpenAiService.
-     */
-    public function getErrorMessage(): ?string
-    {
-        return $this->data['error'] ?? null;
-    }
-
-    /**
-     * Gets the error code returned by OpenAiService.
-     *
-     * @return int|null The error code returned by OpenAiService.
-     */
-    public function getErrorCode(): ?int
-    {
-        return $this->data['code'] ?? null;
-    }
 }
+
---
+++ app/ValueObject/OpenAiResult.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\ValueObject;
+
+/**
+ * Abstract class for representing the result of a request to OpenAiService.
+ */
+abstract class OpenAiResult
+{
+    /**
+     * @var array
+     */
+    protected array $data;
+
+    /**
+     * Constructor.
+     *
+     * @param array $data The data returned by OpenAiService.
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Gets the error message returned by OpenAiService.
+     *
+     * @return string|null The error message returned by OpenAiService.
+     */
+    public function getErrorMessage(): ?string
+    {
+        return $this->data['error'] ?? null;
+    }
+
+    /**
+     * Gets the error code returned by OpenAiService.
+     *
+     * @return int|null The error code returned by OpenAiService.
+     */
+    public function getErrorCode(): ?int
+    {
+        return $this->data['code'] ?? null;
+    }
+}
