985553db039833605d979b02309a3f1075aab494
missingnin <vartanyandg@gmail.com>
2024-10-12 21:58:05 +0300

    FTBE: Add imageUrl Checkout in Image Service | ImageService::isImageUrlAccessible()

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -4,26 +4,50 @@ namespace App\Services;
 
 use App\Models\Product;
 use App\Repositories\ImageRepository;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Support\Collection;
 
 /**
  * Image service class
+ *
+ * This class provides a way to interact with images and synchronize them with products.
+ *
+ * @package App\Services
  */
 class ImageService implements ImageServiceInterface
 {
+    /**
+     * @var ImageRepository Image repository instance
+     */
     private ImageRepository $imageRepository;
 
+    /**
+     * @var Client GuzzleHttp\Client instance
+     */
+    private Client $httpClient;
+
+    /**
+     * Constructor
+     *
+     * Initializes the image repository and HTTP client instances
+     *
+     * @param ImageRepository $imageRepository
+     */
     public function __construct(ImageRepository $imageRepository)
     {
         $this->imageRepository = $imageRepository;
+        $this->httpClient = new Client();
     }
 
     /**
      * Synchronizes images with a product
      *
+     * Synchronizes the images with the product and returns the updated image collection
+     *
      * @param array $images An array of images to synchronize
-     * @param Product $product
-     * @return Collection
+     * @param Product $product The product to synchronize images with
+     * @return Collection The updated image collection
      */
     public function syncImages(array $images, Product $product): Collection
     {
@@ -34,5 +58,24 @@ class ImageService implements ImageServiceInterface
         );
 
         return $product->images()->get();
+    }
+
+    /**
+     * Check if an image is accessible by its URL.
+     *
+     * Sends a HEAD request to the image URL and checks if the response status code is 200
+     *
+     * @param string $imageUrl The image URL to check
+     * @return bool True if the image is accessible, false otherwise
+     */
+    public function isImageUrlAccessible(string $imageUrl): bool
+    {
+        try {
+            $response = $this->httpClient->head($imageUrl);
+
+            return $response->getStatusCode() === 200;
+        } catch (GuzzleException) {
+            return false;
+        }
     }
 }
