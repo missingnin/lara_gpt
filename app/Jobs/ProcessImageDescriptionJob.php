ea1e49f8505187263af3d31d0d46f7ca3608f83b
missingnin <vartanyandg@gmail.com>
2024-10-14 05:36:30 +0300

    FTBE: Refactor | app/Jobs/ProcessImageDescriptionJob.php

--- app/Jobs/ProcessImageDescriptionJob.php
+++ app/Jobs/ProcessImageDescriptionJob.php
@@ -2,21 +2,20 @@
 
 namespace App\Jobs;
 
-use App\Models\Image;
 use App\Repositories\ImageRepository;
-use App\Services\ImageServiceInterface;
+use App\Services\OpenAiInterface;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 /**
- * Class ProcessImageDescriptionJob
- *
- * This job processes the image description for a given image URL and imagesPrompt.
+ * Job to get image description from OpenAI.
  */
-class ProcessImageDescriptionJob extends AbstractJob implements ShouldQueue
+class GetImageDescriptionJob extends AbstractJob implements ShouldQueue
 {
     use Dispatchable;
     use InteractsWithQueue;
@@ -37,72 +36,43 @@ class ProcessImageDescriptionJob extends AbstractJob implements ShouldQueue
      * Constructor.
      *
      * @param string $imageUrl Image URL
-     * @param string $imagesPrompt
+     * @param string $imagesPrompt imagesPrompt for image description
      */
     public function __construct(string $imageUrl, string $imagesPrompt)
     {
         $this->imageUrl = $imageUrl;
         $this->imagesPrompt = $imagesPrompt;
-        $this->logInfo(
-            "Job constructed with image URL: 
-            {$this->imageUrl} 
-            and imagesPrompt: {$this->imagesPrompt}"
-        );
+        $this->logInfo("GetImageDescriptionJob constructed with image URL: {$this->imageUrl}");
     }
 
     /**
-     * Execute the job.
+     * Handle the job.
+     *
+     * @param ImageRepository $imageRepository
+     * @param OpenAiInterface $openAiClient
+     *
+     * @return void
+     * @throws Exception
      */
-    public function handle(ImageServiceInterface $imageService, ImageRepository $imageRepository): void
+    public function handle(ImageRepository $imageRepository, OpenAiInterface $openAiClient): void
     {
-        $this->logInfo('Handling image description processing...');
-        $image = $imageRepository->findByAttribute('name', $this->imageUrl);
-
-        if ($image) {
-            if ($imageRepository->imageNeedDescription($this->imagesPrompt, $image)) {
-                $this->logInfo('Image needs description. Processing...');
-                $this->processImageDescription($image, $imageService, $imageRepository);
-            } else {
-                $this->logInfo('Image already has description. Skipping.');
-            }
-        }
-    }
-
-    /**
-     * Process the image description.
-     *
-     * @param Image $image The image object
-     * @param ImageServiceInterface $imageService The image service instance
-     * @param ImageRepository $imageRepository The image repository instance
-     */
-    private function processImageDescription(
-        Image $image,
-        ImageServiceInterface $imageService,
-        ImageRepository $imageRepository
-    ): void {
-        $this->logInfo('Processing image description...');
-        if ($imageService->isImageUrlAccessible($this->imageUrl)) {
-            $this->logInfo(
-                'Image URL is accessible. Dispatching GetImageDescriptionJob.'
+        $this->logInfo('Handling GetImageDescriptionJob...');
+        try {
+            $imageDescriptionResult = $openAiClient->getImageDescription(
+                $this->imageUrl,
+                $this->imagesPrompt
             );
-            GetImageDescriptionJob::dispatch($this->imageUrl, $this->imagesPrompt);
-        } else {
-            $this->logInfo('Image URL is not accessible. Handling inaccessible image...');
-            $this->handleInaccessibleImage($image, $imageRepository);
-        }
-    }
 
-    /**
-     * Handle the inaccessible image.
-     *
-     * @param Image $image The image object (maybe null)
-     * @param ImageRepository $imageRepository The image repository instance
-     */
-    private function handleInaccessibleImage(
-        Image $image,
-        ImageRepository $imageRepository
-    ): void {
-        $this->logInfo('Image found in repository, but URL Inaccessible - "No Description"');
-        $imageRepository->setDescription($image);
+            if ($imageDescriptionResult->getContent()) {
+                $image = $imageRepository->findByAttribute('name', $imageDescriptionResult->getContent());
+                $imageRepository->setDescription($image, $imageDescriptionResult->getContent());
+                $this->logInfo("Image description set: {$imageDescriptionResult->getContent()}");
+            } else {
+                $this->logWarning("No image description returned from OpenAI");
+            }
+        } catch (Exception $e) {
+            $this->logError("Error getting image description: {$e->getMessage()}");
+            throw $e;
+        }
     }
 }
