6f0873d4f26f09b4cc81047ebc794c5e68abbdcc
missingnin <vartanyandg@gmail.com>
2024-10-14 22:04:02 +0300

    FTBE: Create New Event | app/Events/ImageDescriptionUpdatedEvent.php

---
+++ app/Events/ImageDescriptionUpdatedEvent.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * Event triggered when an image description is updated.
+ */
+class ImageDescriptionUpdatedEvent
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    /**
+     * @var int The product ID
+     */
+    public int $productId;
+
+    /**
+     * @var int The percentage of processed images
+     */
+    public int $percentage;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param int $productId The product ID
+     * @param int $percentage The percentage of processed images
+     */
+    public function __construct(int $productId, int $percentage)
+    {
+        $this->productId = $productId;
+        $this->percentage = $percentage;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('image-description-updated'),
+        ];
+    }
+
+    /**
+     * Log the event.
+     */
+    public function log(): void
+    {
+        Log::info("Image description updated for product ID: {$this->productId} - {$this->percentage}%");
+    }
+}
