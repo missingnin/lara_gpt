2e38c5a604d54b135e1db735e0902da2304a053c
missingnin <vartanyandg@gmail.com>
2024-10-15 05:20:55 +0300

    FTBE: Refactor | app/Services/OtDushiAi/Processors/OtDushiAiProcessor.php

--- app/Services/OtDushiAi/Processors/OtDushiAiProcessor.php
+++ app/Services/OtDushiAi/Processors/OtDushiAiProcessor.php
@@ -8,7 +8,7 @@ use App\Jobs\ProcessImageDescriptionJob;
 use App\Repositories\ProductRepository;
 use App\Services\ImageServiceInterface;
 use InvalidArgumentException;
-use Illuminate\Support\Facades\Log;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class for OtDushi AI processors
@@ -26,17 +26,25 @@ class OtDushiAiProcessor
     private ProductRepository $productRepository;
 
     /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    /**
      * Constructor
      *
      * @param ImageServiceInterface $imageService
      * @param ProductRepository $productRepository
+     * @param LoggerInterface $logger
      */
     public function __construct(
         ImageServiceInterface $imageService,
-        ProductRepository $productRepository
+        ProductRepository $productRepository,
+        LoggerInterface $logger
     ) {
         $this->imageService = $imageService;
         $this->productRepository = $productRepository;
+        $this->logger = $logger;
     }
 
     /**
@@ -48,22 +56,20 @@ class OtDushiAiProcessor
      */
     public function process(array $data, int $processType): void
     {
-        Log::info('Starting processing data with process type ' . $processType);
+        $this->logger->info('Starting processing data with process type ' . $processType);
 
         try {
             match ($processType) {
                 OtDushiAiProcessTypes::GET_AI_IMAGES_DESCRIPTION => $this->processImageDescription($data),
+                OtDushiAiProcessTypes::GET_AI_SPREADS_GROUPS => $this->processSpreadsDescription($data),
                 default => throw new InvalidProcessTypeException($processType),
             };
         } catch (InvalidProcessTypeException $e) {
-            Log::error('Invalid process type: ' . $e->getMessage());
+            $this->logger->error('Invalid process type: ' . $e->getMessage());
             throw $e;
         }
 
-        Log::info(
-            'Finished processing data with process type '
-            . $processType
-        );
+        $this->logger->info('Finished processing data with process type ' . $processType);
     }
 
     /**
@@ -74,7 +80,7 @@ class OtDushiAiProcessor
      */
     private function processImageDescription(array $data): void
     {
-        Log::info('Starting processing image description');
+        $this->logger->info('Starting processing image description');
 
         try {
             if (
@@ -92,14 +98,32 @@ class OtDushiAiProcessor
             $images = $this->imageService->syncImages($data['images_prompt'], $data['images'], $product);
 
             foreach ($images->toArray() as $image) {
-                Log::info('Dispatching ProcessImageDescriptionJob for image ' . $image['name']);
+                $this->logger->info('Dispatching ProcessImageDescriptionJob for image ' . $image['name']);
                 ProcessImageDescriptionJob::dispatch($image['name'], $data['images_prompt']);
             }
         } catch (InvalidArgumentException $e) {
-            Log::error('Invalid data: ' . $e->getMessage());
+            $this->logger->error('Invalid data: ' . $e->getMessage());
             throw $e;
         }
 
-        Log::info('Finished processing image description');
+        $this->logger->info('Finished processing image description');
+    }
+
+    /**
+     * Process spreads description
+     *
+     * @param array $data
+     * @throws InvalidArgumentException
+     */
+    private function processSpreadsDescription(array $data): void
+    {
+        if (!isset($data['product_id'])) {
+            throw new InvalidArgumentException('Missing required product id');
+        }
+
+        $this->logger->info('Retrieving images with descriptions for product id ' . $data['product_id']);
+        $imagesWithDescription = $this->productRepository->getImagesWithDescription($data['product_id']);
+
+        $this->logger->info('Retrieved images with descriptions:', $imagesWithDescription);
     }
 }
