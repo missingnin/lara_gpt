1da7e58c430f9aedcf7158989343f9df1c88d9ec
missingnin <vartanyandg@gmail.com>
2024-10-15 04:57:21 +0300

    FTBE: Change Logging From Single To Daily |  config/logging.php

--- config/logging.php
+++ config/logging.php
@@ -59,10 +59,11 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
+            'days' => 14,
         ],
 
         'daily' => [
@@ -89,7 +90,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
