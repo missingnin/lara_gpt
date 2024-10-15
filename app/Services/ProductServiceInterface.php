0ce39afc964ade3839f198cca5dbef322c6429a1
missingnin <vartanyandg@gmail.com>
2024-10-15 05:55:19 +0300

    FTBE: Create Product Service Contract | NEW app/Services/ProductServiceInterface.php

---
+++ app/Services/ProductServiceInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Interface for product services.
+ */
+interface ProductServiceInterface
+{
+    /**
+     * Synchronizes images with a product.
+     *
+     * @param string $imagesPrompt The prompt for the images
+     * @param array $images        An array of images to synchronize
+     * @param Product $product     The product to synchronize images with
+     *
+     * @return Collection A collection of synchronized images
+     */
+    public function syncImages(string $imagesPrompt, array $images, Product $product): Collection;
+}
