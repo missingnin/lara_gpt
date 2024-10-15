afd45549954f1f3ca10f0f3f36bb59255c9c6c64
missingnin <vartanyandg@gmail.com>
2024-10-15 07:05:11 +0300

    FTBE: Create Spreads Description Result | NEW

---
+++ app/ValueObject/SpreadsDescriptionResult.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\ValueObject;
+
+use App\ValueObject\OpenAiResult;
+
+/**
+ * Class for representing the result of an image description request to OpenAiService.
+ */
+class SpreadsDescriptionResult extends OpenAiResult
+{
+
+}
+
